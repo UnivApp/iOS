1a9a69a1d8821b396ad7c774c0f1ee49ff91f02d
jeoungsung12 <jeoungsung12@naver.com>
2024-08-22 20:08:41 +0900

    init push

---
+++ UnivApp/UnivAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnivAppApp.swift
+//  UnivApp
+//
+//  Created by 정성윤 on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct UnivAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
