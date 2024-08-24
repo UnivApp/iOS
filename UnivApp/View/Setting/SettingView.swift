472c715a61601371e2ed9becdfee22ff970269b1
jeoungsung12 <jeoungsung12@naver.com>
2024-08-24 15:33:32 +0900

    Setting UI 구현

--- UnivApp/View/Setting/SettingView.swift
+++ UnivApp/View/Setting/SettingView.swift
@@ -9,7 +9,108 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text("SettingView")
+        NavigationStack {
+            Spacer()
+            
+            profile
+            
+            Spacer()
+            
+            setting
+                .padding(.bottom, 30)
+            
+            support
+            
+            Spacer()
+        }
+    }
+    
+    var profile: some View {
+        VStack(alignment: .center) {
+            Image("person")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120, height: 100)
+                .padding(.horizontal, 20)
+            
+            Text("성윤님")
+                .foregroundColor(.black)
+                .font(.system(size: 20, weight: .bold))
+                .padding(.top, 10)
+            
+            
+            Text("jeoungsung12@naver.com")
+                .foregroundColor(.black)
+                .font(.system(size: 12, weight: .regular))
+                .padding(.top, 10)
+            
+        }
+    }
+    
+    var setting: some View {
+        VStack(alignment: .leading) {
+            Group {
+                Text("계정 설정")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 10, weight: .bold))
+                    .padding(.leading, 20)
+                    .padding(.bottom, 20)
+                    
+                
+                ForEach(SettingType.allCases, id: \.self) { cases in
+                    NavigationLink(destination: ListView()) {
+                        HStack {
+                            Text("\(cases.title)")
+                                .foregroundColor(.black)
+                                .font(.system(size: 14, weight: .bold))
+                                .padding(.leading, 30)
+                            
+                            Spacer()
+                            
+                            Image("arrow_fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 5, height: 10)
+                                .padding(.trailing, 30)
+                        }
+                        .padding(.bottom, 10)
+                    }
+                }
+            }
+        }
+    }
+    
+    var support: some View {
+        VStack(alignment: .leading) {
+            Group {
+                Text("지원")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 10, weight: .bold))
+                    .padding(.leading, 20)
+                    .padding(.bottom, 20)
+                    
+                
+                ForEach(SupportType.allCases, id: \.self) { cases in
+                    NavigationLink(destination: ListView()) {
+                        HStack {
+                            Text("\(cases.title)")
+                                .foregroundColor(.black)
+                                .font(.system(size: 14, weight: .bold))
+                                .padding(.leading, 30)
+                            
+                            Spacer()
+                            
+                            Image("arrow_fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 5, height: 10)
+                                .padding(.trailing, 30)
+                        }
+                        .padding(.bottom, 10)
+                    }
+                }
+            }
+        }
     }
 }
 
