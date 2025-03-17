131a6e4775c0c99275906ae4272261662a72dd54
jeoungsung12 <jeoungsung12@naver.com>
2025-03-17 20:41:18 +0900

    Add: NetworkManager/Monitor/Router

--- UnivApp/Network/NetworkManager.swift
+++ UnivApp/Network/NetworkManager.swift
@@ -1,8 +1,39 @@
 //
 //  NetworkManager.swift
-//  UnivApp
+//  NaMuApp
 //
-//  Created by 정성윤 on 3/17/25.
+//  Created by 정성윤 on 1/25/25.
 //
 
 import Foundation
+import Alamofire
+import Combine
+
+protocol NetworkManagerType: AnyObject {
+    func getData<T: Decodable, U: Router>(_ api: U) -> AnyPublisher<T,Error>
+}
+
+final class NetworkManager: NetworkManagerType {
+    static let shared = NetworkManager()
+    
+    private init() { }
+    
+    func getData<T: Decodable, U: Router>(_ api: U) -> AnyPublisher<T,Error> {
+//        print(api.endpoint)
+        return Future<T,Error> { promise in
+            AF.request(api)
+                .validate(statusCode: 200...500)
+                .responseDecodable(of: T.self) { response in
+//                    dump(response.debugDescription)
+//                    let statucCode = NetworkError().checkErrorType(response.response?.statusCode)
+                    switch response.result {
+                    case let .success(data):
+                        promise(.success(data))
+                    case let .failure(error):
+                        promise(.failure(error))
+                    }
+                }
+        }.eraseToAnyPublisher()
+    }
+    
+}
